3b755055762331183c6602e61b4ea45721242b5b
Zhanhu079 <keitumetse1233@gmail.com>
2024-01-17 15:55:10 +0200

    finish about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,40 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precision We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="https://images.pexels.com/photos/17066896/pexels-photo-17066896/free-photo-of-minimalistic-interior-design.jpeg" alt="aboutImage" fill />
+        <Image 
+          src="/about.png"
+          alt="about image"
+          fill
+          className={styles.img}
+        />
       </div>
     </div>
   );
